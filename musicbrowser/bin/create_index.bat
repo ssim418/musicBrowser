8147dda5fac2e2f9e915048b028d4822347182b3
sdfsfsf <wefwefwef@gmail.com>
2018-07-22 12:40:47 +0200

    update: paths

--- musicbrowser/bin/create_index.bat
+++ musicbrowser/bin/create_index.bat
@@ -1,3 +1,3 @@
 chcp 65001
-C:\Users\LivingRoom\AppData\Local\Programs\Python\Python35-32\python.exe C:/musicBrowser/musicbrowser/create_index.py
+C:\Users\Shbl\AppData\Local\Programs\Python\Python35-32\python.exe C:\Users\Shbl\PycharmProjects\musicBrowser3\musicbrowser\create_index.py
 pause
--- musicbrowser/bin/update.bat
+++ musicbrowser/bin/update.bat
@@ -1,3 +1,3 @@
-cd C:\musicBrowser\musicbrowser
+cd C:\Users\Shbl\PycharmProjects\musicBrowser3
 "C:\Program Files\Git\bin\git" pull
 pause
